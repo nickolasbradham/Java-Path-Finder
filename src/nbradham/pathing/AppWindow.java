676a3ce5ac26a22d4ebdc9055be4922cb61d79f3
nickolasbradham <nickolasbradham@gmail.com>
2022-10-07 15:30:50 -0600

    Renamed things for clarity and updated documentation.

--- src/nbradham/pathing/AppWindow.java
+++ src/nbradham/pathing/AppWindow.java
@@ -4,27 +4,34 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.lang.reflect.InvocationTargetException;
-
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
-final class PathingApp extends JFrame implements ActionListener {
+/**
+ * Handles GUI elements.
+ * 
+ * @author Nickolas Bradham
+ *
+ */
+final class AppWindow extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 
 	private static final String ACT_OPEN = "open", ACT_RUN = "run", ACT_PAUSE = "pause", ACT_STEP = "step",
 			ACT_RESET = "reset";
 
-	private final VPanel pane = new VPanel();
+	private final Simulation pane = new Simulation();
 	private final JMenuItem runItem = createJMenuItem("Run", ACT_RUN, KeyEvent.VK_R),
 			pauseItem = createJMenuItem("Pause", ACT_PAUSE, KeyEvent.VK_P),
 			stepItem = createJMenuItem("Step", ACT_STEP, KeyEvent.VK_S),
 			resetItem = createJMenuItem("Reset", ACT_RESET, KeyEvent.VK_E);
 
-	private PathingApp() {
+	/**
+	 * Constructs a new AppWindow instance.
+	 */
+	private AppWindow() {
 		super("Pathing App");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -43,6 +50,14 @@ final class PathingApp extends JFrame implements ActionListener {
 		pack();
 	}
 
+	/**
+	 * Creates a new JMenuItem instance and sets it up.
+	 * 
+	 * @param txt The text displayed by the item.
+	 * @param act The action command of the item.
+	 * @param key The {@link KeyEvent} VK code of the accelerator key.
+	 * @return The new JMenuItem instance.
+	 */
 	private JMenuItem createJMenuItem(String txt, String act, int key) {
 		JMenuItem jmi = new JMenuItem(txt);
 		jmi.setActionCommand(act);
@@ -65,10 +80,7 @@ final class PathingApp extends JFrame implements ActionListener {
 			pauseItem.setEnabled(true);
 			stepItem.setEnabled(false);
 			resetItem.setEnabled(false);
-			if (pane.isStarted())
-				pane.pause();
-			else
-				pane.start();
+			pane.run();
 			break;
 		case ACT_PAUSE:
 			pane.pause();
@@ -86,7 +98,12 @@ final class PathingApp extends JFrame implements ActionListener {
 		}
 	}
 
-	public static void main(String[] args) throws InvocationTargetException, InterruptedException {
-		SwingUtilities.invokeAndWait(() -> new PathingApp().setVisible(true));
+	/**
+	 * Constructs a new AppWindow on the AWT thread and sets it to visible.
+	 * 
+	 * @param args Ignored.
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> new AppWindow().setVisible(true));
 	}
 }
--- src/nbradham/pathing/Simulation.java
+++ src/nbradham/pathing/Simulation.java
@@ -6,29 +6,46 @@ import java.lang.Thread.State;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-final class VPanel extends JPanel {
+/**
+ * Handles simulation execution and drawing.
+ * 
+ * @author Nickolas Bradham
+ *
+ */
+final class Simulation extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	private final SimThread thread = new SimThread();
 	private byte size = 0;
 
-	final void start() {
-		thread.start();
+	/**
+	 * Starts (or resumes) simulation thread.
+	 */
+	final void run() {
+		if (thread.getState() == State.NEW)
+			thread.start();
+		else
+			thread.unpause();
 	}
 
+	/**
+	 * Pauses the simulation thread.
+	 */
 	final void pause() {
 		thread.pause();
 	}
 
+	/**
+	 * Continues the simulation by one step.
+	 */
 	final void step() {
 		size++;
 		SwingUtilities.invokeLater(() -> repaint());
 	}
 
-	final boolean isStarted() {
-		return thread.getState() != State.NEW;
-	}
-	
+	/**
+	 * Resets the simulation to the initial state.
+	 */
 	final void reset() {
 		size = 0;
 	}
@@ -39,16 +56,32 @@ final class VPanel extends JPanel {
 		g.fillRect(0, 0, size * 10, size * 10);
 	}
 
+	/**
+	 * Handles simulation auto stepping.
+	 * 
+	 * @author Nickolas Bradham
+	 *
+	 */
 	private final class SimThread extends Thread {
 
 		private final Object lock = new Object();
 		private boolean pause = false;
 
+		/**
+		 * Pauses the simulation thread.
+		 */
 		private void pause() {
-			if (!(pause = !pause))
-				synchronized (lock) {
-					lock.notify();
-				}
+			pause = true;
+		}
+
+		/**
+		 * Resumes the simulation thread.
+		 */
+		private void unpause() {
+			pause = false;
+			synchronized (lock) {
+				lock.notify();
+			}
 		}
 
 		@Override
